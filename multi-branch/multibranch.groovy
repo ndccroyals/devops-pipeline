080a41af06d72a68247b84939cf19bd6ed096402
JAHNAVI CHAMARTHI <jahnavic@JAHNAVIs-MacBook-Pro.local>
2019-06-06 23:06:42 -0500

    added

---
+++ multi-branch/multibranch.groovy
@@ -0,0 +1,63 @@
+branchRegex = '.*'
+def gitRepoNames= [
+'ndcc-service-registration'
+]
+def repoRoot='https://github.com/ndccroyals/'
+gitRepoNames.each{
+    repo ->
+
+        multibranchPipelineJob(repo+'-multi-branch') {  //predefined method to create multibranch job
+            displayName repo+'-multi-branch'
+
+            triggers {
+                periodic(15)
+            }
+
+            branchSources {
+                configure { node ->
+                    def sourceList = node / sources(class: 'jenkins.branch.MultiBranchProject\$BranchSourceList')
+                    sourceList << 'data' {
+                        'jenkins.branch.BranchSource' {
+                            source(class: 'jenkins.plugins.git.GitSCMSource') {
+                                credentialsId 'ndccroyals:Royals211016'
+                                remote repoRoot + repo + '.git'
+                                ignoreOnPushNotifications
+                                traits {
+                                    'jenkins.scm.impl.trait.RegexSCMHeadFilterTrait' {
+                                        regex(branchRegex)
+                                    }
+                                    'jenkins.plugins.git.traits.BranchDiscoveryTrait'()
+                                    'jenkins.plugins.git.traits.TagDiscoveryTrait'()
+                                }
+                            }
+                        }
+
+                    }
+                    sourceList << owner("", reference: '../..', class: 'org.jenkinsci.plugins.workflow.multibranch.WorkflowMultiBranchProject')
+                }
+            }
+            orphanedItemStrategy {
+                discardOldItems {
+                    daysToKeep(-1)
+                    numToKeep(-1)
+                }
+            }
+
+        }
+
+}
+listView("CT Multibranch Pipeline") {
+    description("CT Multibranch Pipeline")
+    jobs {
+        regex('.+-multi-branch')
+    }
+    columns {
+        status()
+        weather()
+        name()
+        lastSuccess()
+        lastFailure()
+        lastDuration()
+        buildButton()
+    }
+}
